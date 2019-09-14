35ff3f56977dba93ddb3e649abd79c9cf441ff34
Gene H Li <ghl234@nyu.edu>
2019-09-14 00:50:52 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -22,6 +22,10 @@ def using_concat(array1, array2)
   array1.concat(array2)
 end
 
-def using_insert(array1, element)
-  array1. 
+def using_insert(array, element)
+  array.insert(4, element)
+end
+
+def using_uniq(array)
+  array.
   
