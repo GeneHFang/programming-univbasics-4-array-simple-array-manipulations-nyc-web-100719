07f4fcdba93efbae006297e1dbb09d789cce81d2
Gene H Li <ghl234@nyu.edu>
2019-09-14 00:53:08 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -35,6 +35,12 @@ def using_flatten(array)
 end
 
 def using_delete(array,string)
-  array.delete
+  array.delete(string)
+end
+
+def using_delete_at(array, integer)
+  array.delete(integer)
+end
+
 
   
